9e09fd34e0a100cab321d7a8d011673d18fcdcb0
Timothy Fang <timothyfang@Timothys-MacBook-Pro.local>
2025-09-11 21:58:25 -0700

    eod push

--- timfolio/src/pages/Software/SoftwarePage.jsx
+++ timfolio/src/pages/Software/SoftwarePage.jsx
@@ -4,6 +4,7 @@ import ExperienceSection from "../../components/Software/ExperienceSection";
 import ResumeModal from "../../components/Software/ResumeModal";
 import Marquee from "../../components/common/Marquee";
 import jobData from "../../assets/Software/JobExperience.json";
+import Picturesque from "../../assets/Software/picturesque.png"
 
 const SoftwarePage = () => {
     const [isMobile, setIsMobile] = useState(false);
@@ -14,13 +15,13 @@ const SoftwarePage = () => {
         const checkIfMobile = () => {
             setIsMobile(window.innerWidth < 768); // 768px is the md breakpoint in Tailwind
         };
-        
+
         // Initial check
         checkIfMobile();
-        
+
         // Add event listener for window resize
         window.addEventListener('resize', checkIfMobile);
-        
+
         // Cleanup
         return () => window.removeEventListener('resize', checkIfMobile);
     }, []);
@@ -64,7 +65,7 @@ const SoftwarePage = () => {
             <div className="bg-[#FDF7EC] py-10 md:py-20">
                 <Marquee className="mb-10 md:mb-20" text="EXPERIENCE" speed={150} />
                 {jobData.map((job, index) => (
-                    <ExperienceSection 
+                    <ExperienceSection
                         key={job.Title}
                         title={job.Title}
                         company={job.Company}
@@ -77,10 +78,92 @@ const SoftwarePage = () => {
                     />
                 ))}
             </div>
-            <ResumeModal 
+            <ResumeModal
                 isOpen={isResumeOpen}
                 onClose={() => setIsResumeOpen(false)}
             />
+
+            <div className="bg-[#FDF7EC] py-10 md:py-20">
+                <Marquee className="mb-10 md:mb-20" text="PROJECTS" speed={150} />
+            </div>
+            <div className="flex flex-col md:grid md:grid-cols-12 pt-20 md:pt-28 pb-12 md:pb-28 bg-[#FDF7EC] px-10 md:-px-0">
+                <div className="md:col-span-6 order-1 mb-8 md:mb-0">
+                    <div className="relative flex justify-center">
+                        <div className="absolute top-[-30px] left-4 md:top-[-50px] md:right-[50px] bg-[#4A4139] aspect-[4/3] w-[85%]" />
+                        <img
+                            src={Picturesque}
+                            alt="About me visual"
+                            className="aspect-[4/3] w-[85%] object-cover mx-auto md:ml-auto relative"
+                        />
+                    </div>
+                </div>
+                <div className="md:col-span-6 order-2 h-full flex items-start px-4 md:pl-20">
+                    <div className="flex flex-col justify-center items-center">
+                        <div className="relative">
+                            <div className="relative">
+                                <span className="absolute top-2 md:top-4 left-5 bg-[#FDB50B]/60 md:inset-y-10 inset-y-6">
+                                    <h2 className="font-['Cormorant_Garamond'] text-[50px] md:text-[80px] font-medium italic invisible ">
+                                        PICTURESQUE
+                                    </h2>
+                                </span>
+                                <h2 className="font-['Cormorant_Garamond'] text-[50px] md:text-[80px] font-light italic relative -mt-4">
+                                    PICTURESQUE
+                                </h2>
+                            </div>
+                        </div>
+
+                        <div className="mt-5 max-w-xl">
+                            <p className="font-['Roboto'] font-extralight text-[12px] md:text-[14px] lg:text-base leading-relaxed mb-6">
+                                Hi, I'm Timothy! I'm a software engineer passionate about building meaningful, user-focused solutions. My journey as a Chinese-Canadian immigrant has shaped my curiosity and resilience, which I bring into coding, exploring transformative ideas, and personal challenges. I thrive at the intersection of creativity and logic, with experience in full-stack development and a love for learning new technologies.
+                            </p>
+
+                            <p className="font-['Roboto'] font-extralight text-[12px] md:text-[14px] lg:text-base  leading-relaxed">
+                                Beyond tech, I'm a triathlete-in-training, aiming to complete an Ironman. I enjoy experimenting with gaming setups, running marathons, and getting lost in Stardew Valley. I believe small steps create big change—whether it's shipping a feature, mastering a skill, or pushing through the last mile. Let's build something impactful together!
+                            </p>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+            <div className="flex flex-col md:grid md:grid-cols-12 pt-20 md:pt-28 pb-12 md:pb-28 bg-[#FDF7EC] px-10 md:-px-0">
+            <div className="md:col-span-6 order-1 h-full flex items-start px-4 md:pl-20">
+                    <div className="flex flex-col justify-center items-center">
+                        <div className="relative">
+                            <div className="relative">
+                                <span className="absolute top-2 md:top-4 left-5 bg-[#FDB50B]/60 md:inset-y-10 inset-y-6">
+                                    <h2 className="font-['Cormorant_Garamond'] text-[50px] md:text-[80px] font-medium italic invisible ">
+                                        PICTURESQUE
+                                    </h2>
+                                </span>
+                                <h2 className="font-['Cormorant_Garamond'] text-[50px] md:text-[80px] font-light italic relative -mt-4">
+                                    PICTURESQUE
+                                </h2>
+                            </div>
+                        </div>
+
+                        <div className="mt-5 max-w-xl">
+                            <p className="font-['Roboto'] font-extralight text-[12px] md:text-[14px] lg:text-base leading-relaxed mb-6">
+                                Hi, I'm Timothy! I'm a software engineer passionate about building meaningful, user-focused solutions. My journey as a Chinese-Canadian immigrant has shaped my curiosity and resilience, which I bring into coding, exploring transformative ideas, and personal challenges. I thrive at the intersection of creativity and logic, with experience in full-stack development and a love for learning new technologies.
+                            </p>
+
+                            <p className="font-['Roboto'] font-extralight text-[12px] md:text-[14px] lg:text-base  leading-relaxed">
+                                Beyond tech, I'm a triathlete-in-training, aiming to complete an Ironman. I enjoy experimenting with gaming setups, running marathons, and getting lost in Stardew Valley. I believe small steps create big change—whether it's shipping a feature, mastering a skill, or pushing through the last mile. Let's build something impactful together!
+                            </p>
+                        </div>
+
+                    </div>
+                </div>
+                <div className="md:col-span-6 order-2 mb-8 md:mb-0">
+                    <div className="relative flex justify-center">
+                        <div className="absolute top-[-30px] left-4 md:top-[-50px] md:right-[50px] bg-[#4A4139] aspect-[4/3] w-[70%]" />
+                        <img
+                            src={Picturesque}
+                            alt="About me visual"
+                            className="aspect-[4/3] w-[70%] object-cover mx-auto md:ml-auto relative"
+                        />
+                    </div>
+                </div>
+            </div>
         </PageLayout>
     )
 }
